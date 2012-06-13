185fb4841a783233a3f031f89b6ee859b07cb918
Raffael Thome <thome.raffael@gmail.com>
2012-06-13 06:19:03 +0200

    Add a few files

---
+++ EasyLog/ILogFormatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyLog
+{
+    public interface ILogFormatter
+    {
+        string Format(string line, IEnumerable<object> parameters);
+    }
+}
---
+++ EasyLog/Log.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EasyLog.Formatters;
+
+namespace EasyLog
+{
+    public class Log
+    {
+        List<ILogWriter> writers;
+
+        public ILogFormatter Formatter { get; set; }
+
+        public void AddBackend(ILogWriter writer)
+        {
+            if (writer == null)
+                throw new ArgumentNullException("backend", "The given log writer must not be null.");
+
+            writers.Add(writer);
+        }
+
+        public Log()
+        {
+            writers = new List<ILogWriter>();
+            Formatter = new DefaultFormatter();
+        }
+
+        public Log(IEnumerable<ILogWriter> writers)
+            : this()
+        {
+            foreach(var writer in writers)
+                AddBackend(writer);
+        }
+
+        public Log(params ILogWriter[] writers)
+            : this(writers as IEnumerable<ILogWriter>)
+        {
+        }
+    }
+}
